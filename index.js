bb5d4afd82ddb1f8f3bcd560479eda7fe6b43a2d
HenroWasTaken <69813969+HenroWasTaken@users.noreply.github.com>
2021-08-15 13:54:42 -0400

    made website updates

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 document.getElementById("getDownload").onclick = function() {
     window.location.replace("https://github.com/HenroWasTaken/vs-ez-fnf");
 }
+
+document.getElementById("getGitHubProfCIZ").onclick = function() {
+    window.Location.replace("https://github.com/HenroWasTaken");
+}
